cb6a49137bef56634e27971434e22b066bf04c87
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-05-13 19:56:50 -0300

    Inicio Validacion Formulario

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const name = document.getElementById("name");
+const lastName = document.getElementById("lastName");
+const dni = document.getElementById("dni");
+const birthDate = document.getElementById("birthDate");
+const gender = document.getElementsByName ("gender");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const service = document.getElementById("services");
+const appointmentDate = document.getElementById("appointmentDate");
+const button = document.querySelector("button");
+
+
+button.addEventListener("click", (e) => { 
+    event.preventDefault()
+    
+        console.log(name.value)
+        
+
+            
+}
+) 
