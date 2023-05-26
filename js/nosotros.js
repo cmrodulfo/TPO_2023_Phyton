d6688821e958eff78a919720c24a9ffb141e4748
cmrodulfo <cmarin_rodulfo@yahoo.com>
2023-05-25 21:44:22 -0300

    pagina nosotros consumo API

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,17 +1,29 @@
 
-const personData = async () => {
-    try {
-        const data = await fetch ('https://randomuser.me/api/?gender=female&inc=name,nat,email,picture')
-        const dataJson = await data.json()
-        console.log(dataJson);
-    } catch (error) {
-        
-    }
-} 
 
 //personData()
 
 
 const image = document.getElementById("image")
+const profileName = document.getElementById("profileName")
+console.log(profileName.innerHTML);
 
-image.src = personData().results.picture.medium
+
+
+
+
+const personData = async () => {
+    try {
+        const data = await fetch ('https://randomuser.me/api/?gender=female&inc=name,nat,email,picture')
+        const response = await data.json()
+        const person = response.results[0]
+        console.log(person);
+        image.src = person.picture.large
+        profileName.innerHTML = "Dra. "+ person.name.first +" "+ person.name.last
+
+    } catch (error) {
+        
+    }
+} 
+
+const data = personData()
+console.log(data)
